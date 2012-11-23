c6cb4c7ce36f2d36781ec60487d10b0404bb0f51
seth-- <xd.seth@gmail.com>
2012-11-23 00:51:20 -0300

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ multifile.js
 ============
 
 A simple javascript file to upload multiple files showing only one input element
+
+Have an input with id="multifile_input" and type="file". A name finishing in "[]" is needed in php to have the files in $_FILES.
+
+Also, have an ul with id="multifile_list", this is where the list of files will be.
---
+++ multifile.js
@@ -0,0 +1,34 @@
+$('#multifile_input').change(function(){
+	//create a new input
+	new_input = $(this).clone(true);
+	//remove event handlers
+	new_input.off();
+	//make it invisible
+	$(new_input).hide();
+	//insert it after the last input
+	$(this.parentNode).append(new_input);
+	//clean the visible input
+	this.value = '';
+
+	//create a new li element to list the new file
+	new_li = document.createElement('li');
+	//copy the file name
+	new_li.innerHTML = $(new_input).val();
+
+	//create the link to remove the file from the list
+	delete_link = document.createElement('a');
+	delete_link.innerHTML = 'x';
+	delete_link.href = '#';
+	//save a reference to the input containing the file
+	delete_link.input_reference = new_input;
+	//set the handler for the onclick event
+	$(delete_link).click(function(){
+		$(this.input_reference).remove();
+		$(this.parentNode).remove();
+	});
+
+	//put the x in the start of the li
+	$(new_li).prepend(delete_link);
+	//put the li in the end of the container
+	$('#multifile_list').append(new_li);
+});
